180b31d13d797ccb3f877aea0bcd423413de32f0
ilakonovalov <antonpuckich@gmail.com>
2023-07-18 12:53:46 +0300

    Passing model from controller to view

--- spring-mvc/src/main/java/ru/turbogoose/controllers/FirstController.java
+++ spring-mvc/src/main/java/ru/turbogoose/controllers/FirstController.java
@@ -1,27 +1,44 @@
 package ru.turbogoose.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        System.out.println(name + " " + surname);
+    public String helloPage(@RequestParam(required = false) String name,
+                            @RequestParam(required = false) String surname,
+                            Model model) {
+        String message = composeMessage("Hello", name, surname);
+        model.addAttribute("message", message);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(@RequestParam("name") String personName,
-                              @RequestParam(required = false) String surname) {
-        System.out.println(personName + " " + surname);
+    public String goodbyePage(@RequestParam(required = false) String name,
+                              @RequestParam(required = false) String surname,
+                              Model model) {
+        String message = composeMessage("Goodbye", name, surname);
+        model.addAttribute("message", message);
         return "first/goodbye";
+    }
+
+    private String composeMessage(String phrase, String name, String surname) {
+        StringBuilder sb = new StringBuilder(phrase);
+        if (name != null || surname != null) {
+            sb.append(",");
+        }
+        if (name != null) {
+            sb.append(" ").append(name);
+        }
+        if (surname != null) {
+            sb.append(" ").append(surname);
+        }
+        sb.append("!");
+        return sb.toString();
     }
 }
