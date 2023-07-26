80ef3645ea86a59884a9444a0cbb416c73354e86
ilakonovalov <antonpuckich@gmail.com>
2023-07-26 20:58:58 +0300

    Project 1: add dependencies and java based configs

---
+++ project-1-library/src/main/java/project/lib/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,28 @@
+package project.lib.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[] {new HiddenHttpMethodFilter()};
+    }
+}
