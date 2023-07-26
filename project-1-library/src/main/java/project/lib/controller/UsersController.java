5464b8fe15f388e3817051643d9e219c48ca4d1f
ilakonovalov <antonpuckich@gmail.com>
2023-07-26 21:13:32 +0300

    Project 1: add Person field validations

--- project-1-library/src/main/java/project/lib/controller/UsersController.java
+++ project-1-library/src/main/java/project/lib/controller/UsersController.java
@@ -2,6 +2,7 @@ package project.lib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import project.lib.dao.PersonDao;
 import project.lib.model.Person;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -34,7 +36,10 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createPerson(@ModelAttribute Person person) {
+    public String createPerson(@Valid @ModelAttribute Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/create";
+        }
         int id = dao.save(person);
         return "redirect:/people/" + id;
     }
--- project-1-library/src/main/java/project/lib/model/Person.java
+++ project-1-library/src/main/java/project/lib/model/Person.java
@@ -3,10 +3,15 @@ package project.lib.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 public class Person {
     private int id;
+    @NotBlank(message = "Full name must not be blank")
     private String fullName;
+    @Min(value = 1900, message = "Year of birth must be greater than 1900")
     private int yearOfBirth;
 }
