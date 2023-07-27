8d0e8ba912db0bad50a271a81e7a2033989e9c04
ilakonovalov <antonpuckich@gmail.com>
2023-07-27 19:19:15 +0300

    Project 1: implement book creation logic

--- project-1-library/src/main/java/project/lib/controller/BooksController.java
+++ project-1-library/src/main/java/project/lib/controller/BooksController.java
@@ -2,10 +2,15 @@ package project.lib.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.lib.dao.BookDao;
 import project.lib.model.Book;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/books")
@@ -28,5 +33,12 @@ public class BooksController {
         return "books/create";
     }
 
-
+    @PostMapping
+    public String createBook(@Valid @ModelAttribute Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "books/create";
+        }
+        int id = bookDao.save(book);
+        return "redirect:/books/" + id;
+    }
 }
--- project-1-library/src/main/java/project/lib/dao/BookDao.java
+++ project-1-library/src/main/java/project/lib/dao/BookDao.java
@@ -2,10 +2,16 @@ package project.lib.dao;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import project.lib.model.Book;
+import project.lib.model.Person;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class BookDao {
@@ -22,5 +28,30 @@ public class BookDao {
     public List<Book> getBooksByPersonId(int personId) {
         final String SQL = "SELECT * FROM Book b join Person p on b.person_id = p.id WHERE person_id=?";
         return jdbcTemplate.query(SQL, new BeanPropertyRowMapper<>(Book.class), personId);
+    }
+
+    public int save(Book book) {
+        final String SQL = "INSERT INTO Book(title, author, year_of_publication) VALUES(?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(conn -> {
+            PreparedStatement ps = conn.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, book.getTitle());
+            ps.setString(2, book.getAuthor());
+            ps.setInt(3, book.getYearOfPublication());
+            return ps;
+        }, keyHolder);
+
+        int generatedId = extractGeneratedId(keyHolder, "id");
+        book.setId(generatedId);
+        return generatedId;
+    }
+
+    private int extractGeneratedId(KeyHolder keyHolder, String idFieldName) {
+        Map<String, Object> generatedKeys = keyHolder.getKeys();
+        if (generatedKeys == null || !generatedKeys.containsKey(idFieldName)) {
+            throw new RuntimeException("Id was not generated");
+        }
+        return (int) generatedKeys.get(idFieldName);
     }
 }
