af9fa0f66e0814e8978e75e4eb64bdd7e56fbba2
ilakonovalov <antonpuckich@gmail.com>
2023-07-29 21:49:57 +0300

    Refactor previous example

--- hibernate/src/main/java/ru/hibernate/model/Person.java
+++ hibernate/src/main/java/ru/hibernate/model/Person.java
@@ -1,22 +1,16 @@
 package ru.hibernate.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name = "person")
 public class Person {
     @Id
-    @Column(name = "id")
     private int id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "age")
     private int age;
 }
