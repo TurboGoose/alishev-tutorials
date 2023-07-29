0da3892b99a183b5ff7d20fdb54a5519861e71e1
ilakonovalov <antonpuckich@gmail.com>
2023-07-29 22:24:58 +0300

    Implement id generation with sequence strategy

--- hibernate/src/main/java/ru/hibernate/model/Person.java
+++ hibernate/src/main/java/ru/hibernate/model/Person.java
@@ -1,16 +1,16 @@
 package ru.hibernate.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "person_id_seq_generator")
+    @SequenceGenerator(name = "person_id_seq_generator",
+            sequenceName = "person_id_seq", allocationSize = 1)
     private int id;
     private String name;
     private int age;
