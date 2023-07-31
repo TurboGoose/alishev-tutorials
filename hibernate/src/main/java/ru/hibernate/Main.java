04ee0577d7fd83c4f501ae4c8c6fea5da78431ee
ilakonovalov <antonpuckich@gmail.com>
2023-07-31 19:12:14 +0300

    Practicing HQL queries

--- hibernate/src/main/java/ru/hibernate/Main.java
+++ hibernate/src/main/java/ru/hibernate/Main.java
@@ -4,7 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.hibernate.model.Person;
-import ru.hibernate.util.PersonFactory;
+
+import java.util.List;
 
 class Main {
     public static void main(String[] args) {
@@ -13,26 +14,14 @@ class Main {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Person person = PersonFactory.get();
+            List<Person> people = session.createQuery("FROM Person WHERE age < 30", Person.class).getResultList();
+            people.forEach(System.out::println);
 
-            // create
-            session.persist(person);
-            int id = person.getId();
-            System.out.println("Generated id: " + id);
+            session.createQuery("update Person set age = 18 where age < 18 and age >= 14", Person.class).executeUpdate();
 
-            // read
-            person = session.get(Person.class, id);
-            System.out.println("Loaded person: " + person);
-
-            // update
-            person.setName("#" + person.getName());
-            System.out.println("After update");
-
-            // delete
-//            session.remove(person);
+            session.createQuery("delete from Person where age < 14", Person.class).executeUpdate();
 
             session.getTransaction().commit();
-            System.out.println("After transaction");
         }
     }
 }
