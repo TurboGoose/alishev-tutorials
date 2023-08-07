a61677f9ff57972abaa24f11ab5e577259193190
ilakonovalov <antonpuckich@gmail.com>
2023-08-07 16:24:35 +0300

    Add JPA annotations to models

--- project-library/src/main/java/project/lib/model/Book.java
+++ project-library/src/main/java/project/lib/model/Book.java
@@ -1,18 +1,31 @@
 package project.lib.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotBlank(message = "Title must not be blank")
     private String title;
     @NotBlank(message = "Author name must not be blank")
     private String author;
     @Min(value = 0, message = "Year of publication must be positive or zero")
+    @Column(name = "year_of_publication")
     private int yearOfPublication;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person borrower;
 }
--- project-library/src/main/java/project/lib/model/Person.java
+++ project-library/src/main/java/project/lib/model/Person.java
@@ -1,16 +1,31 @@
 package project.lib.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotBlank(message = "Full name must not be blank")
+    @Column(name = "full_name")
     private String fullName;
     @Min(value = 1900, message = "Year of birth must be greater than 1900")
+    @Column(name = "year_of_birth")
     private int yearOfBirth;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToMany(mappedBy = "borrower")
+    private List<Book> books;
 }
