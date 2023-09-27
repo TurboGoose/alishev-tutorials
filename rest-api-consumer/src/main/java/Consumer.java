a86573108f109f886ade7cf0ca312a86b4e21da3
ilakonovalov <antonpuckich@gmail.com>
2023-09-27 14:51:29 +0300

    Post request example

--- rest-api-consumer/src/main/java/Consumer.java
+++ rest-api-consumer/src/main/java/Consumer.java
@@ -1,10 +1,21 @@
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 public class Consumer {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
-        String uri = "https://reqres.in/api/users/1";
-        String json = restTemplate.getForObject(uri, String.class);
-        System.out.println(json);
+
+        Map<String, String> requestBody = Map.of(
+                "name", "Ilya",
+                "job", "Java developer"
+        );
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody);
+
+        String uri = "https://reqres.in/api/users";
+        String responseBody = restTemplate.postForObject(uri, request, String.class);
+        System.out.println(responseBody);
     }
 }
