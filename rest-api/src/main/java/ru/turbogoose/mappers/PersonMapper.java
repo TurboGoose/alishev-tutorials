72fd505184742b19c72a627e91d0fc99dd285320
ilakonovalov <antonpuckich@gmail.com>
2023-09-29 10:44:47 +0300

    Add ModelMapper for object mapping

--- rest-api/src/main/java/ru/turbogoose/mappers/PersonMapper.java
+++ rest-api/src/main/java/ru/turbogoose/mappers/PersonMapper.java
@@ -1,26 +1,22 @@
 package ru.turbogoose.mappers;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import ru.turbogoose.dto.PersonRequestDto;
 import ru.turbogoose.dto.PersonResponseDto;
 import ru.turbogoose.model.Person;
 
 @Component
+@RequiredArgsConstructor
 public class PersonMapper {
+    private final ModelMapper modelMapper;
+
     public PersonResponseDto toDto(Person person) {
-        PersonResponseDto dto = new PersonResponseDto();
-        dto.setId(person.getId());
-        dto.setAge(person.getAge());
-        dto.setName(person.getName());
-        dto.setEmail(person.getEmail());
-        return dto;
+        return modelMapper.map(person, PersonResponseDto.class);
     }
 
     public Person toModel(PersonRequestDto personRequestDto) {
-        Person model = new Person();
-        model.setAge(personRequestDto.getAge());
-        model.setName(personRequestDto.getName());
-        model.setEmail(personRequestDto.getEmail());
-        return model;
+        return modelMapper.map(personRequestDto, Person.class);
     }
 }
