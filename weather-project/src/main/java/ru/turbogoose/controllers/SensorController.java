20a1f552f82e7b44d89067daa58ee2b7088debdc
ilakonovalov <antonpuckich@gmail.com>
2023-09-29 20:44:45 +0300

    Implement endpoint for getting all sensors

--- weather-project/src/main/java/ru/turbogoose/controllers/SensorController.java
+++ weather-project/src/main/java/ru/turbogoose/controllers/SensorController.java
@@ -2,15 +2,13 @@ package ru.turbogoose.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.turbogoose.dto.SensorDto;
 import ru.turbogoose.mappers.SensorMapper;
 import ru.turbogoose.services.SensorService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sensors")
@@ -20,8 +18,15 @@ public class SensorController {
     private final SensorService sensorService;
 
     @PostMapping
-    public ResponseEntity<HttpStatus> registerSensor(@RequestBody @Valid SensorDto sensorDto) {
+    public ResponseEntity<Void> registerSensor(@RequestBody @Valid SensorDto sensorDto) {
         sensorService.registerSensor(sensorMapper.toModel(sensorDto));
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public List<SensorDto> getAllSensors() {
+        return sensorService.getAllSensors().stream()
+                .map(sensorMapper::toDto)
+                .toList();
     }
 }
